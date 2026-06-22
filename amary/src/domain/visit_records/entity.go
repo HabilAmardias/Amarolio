2032725c7559d6e7b28f6a37d3a0db6828000c6a
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2026-06-22 18:09:00 +0700

    feat: update amary service to add dashboard summary endpoint

--- amary/src/domain/visit_records/entity.go
+++ amary/src/domain/visit_records/entity.go
@@ -19,4 +19,47 @@ type (
 		Device string
 		Time   string
 	}
+	DeviceCount struct {
+		Device string
+		Count  int64
+	}
+
+	DayOfWeekCount struct {
+		DayOfWeek string
+		Count     int64
+	}
+	DeviceDayOfWeekCount struct {
+		Device    string
+		DayOfWeek string
+		Count     int64
+	}
+	VisitDashboard struct {
+		TodayVisitCount        int64
+		ThisWeekCount          int64
+		TodayDeviceCount       []DeviceCount
+		ThisDayOfWeekCount     []DayOfWeekCount
+		ThisWeekDeviceCount    []DeviceCount
+		ThisWeekDOWDeviceCount []DeviceDayOfWeekCount
+	}
+	TaggedDeviceCount struct {
+		Device string `json:"device"`
+		Count  int64  `json:"count"`
+	}
+	TaggedDayOfWeekCount struct {
+		DayOfWeek string `json:"day_of_week"`
+		Count     int64  `json:"count"`
+	}
+	TaggedDeviceDayOfWeekCount struct {
+		Device    string `json:"device"`
+		DayOfWeek string `json:"day_of_week"`
+		Count     int64  `json:"count"`
+	}
+	TaggedVisitDashboard struct {
+		TodayVisitCount        int64                        `json:"today_visit_count"`
+		ThisWeekCount          int64                        `json:"this_week_count"`
+		TodayDeviceCount       []TaggedDeviceCount          `json:"today_device_count"`
+		ThisDayOfWeekCount     []TaggedDayOfWeekCount       `json:"this_day_of_week_count"`
+		ThisWeekDeviceCount    []TaggedDeviceCount          `json:"this_week_device_count"`
+		ThisWeekDOWDeviceCount []TaggedDeviceDayOfWeekCount `json:"this_week_dow_device_count"`
+	}
 )
---
+++ amary/src/domain/visit_records/service.go
@@ -0,0 +1,132 @@
+package visitrecords
+
+import (
+	"amary/src/customerror"
+	"amary/src/domain/url"
+	"amary/src/services"
+	"context"
+	"errors"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type VisitRecordRepoItf interface {
+	GetThisWeekCountGroupByDeviceAndDayOfWeek(ctx context.Context, urlID int64, count *[]DeviceDayOfWeekCount) error
+	GetThisWeekCountGroupByDevice(ctx context.Context, urlID int64, count *[]DeviceCount) error
+	GetThisWeekCount(ctx context.Context, urlID int64, count *int64) error
+	GetThisDayOfWeekCount(ctx context.Context, urlID int64, weekCounts *[]DayOfWeekCount) error
+	GetTodayCountGroupByDevice(ctx context.Context, urlID int64, count *[]DeviceCount) error
+	GetTodayCount(ctx context.Context, urlID int64, count *int64) error
+}
+
+type VisitRecordCacheItf interface {
+	Set(ctx context.Context, code string, ttl time.Duration, vd VisitDashboard) error
+	Get(ctx context.Context, code string, vd *VisitDashboard) error
+}
+
+type URLRepoItf interface {
+	FindByCode(ctx context.Context, shortCode string, url *url.URL) error
+}
+
+type URLCacheItf interface {
+	Get(ctx context.Context, encodedID string, url *url.URL) error
+}
+
+type VisitRecordServiceImpl struct {
+	vrr VisitRecordRepoItf
+	ur  URLRepoItf
+	uc  URLCacheItf
+	vc  VisitRecordCacheItf
+}
+
+func NewVisitRecordService(vrr VisitRecordRepoItf, ur URLRepoItf, uc URLCacheItf, vc VisitRecordCacheItf) *VisitRecordServiceImpl {
+	return &VisitRecordServiceImpl{vrr, ur, uc, vc}
+}
+
+func (vrs *VisitRecordServiceImpl) GetVisitRecordSummary(ctx context.Context, userID string, code string) (*VisitDashboard, error) {
+	var (
+		todayVisitCount        *int64                  = new(int64)
+		thisWeekCount          *int64                  = new(int64)
+		todayDeviceCount       *[]DeviceCount          = new([]DeviceCount)
+		thisDayOfWeekCount     *[]DayOfWeekCount       = new([]DayOfWeekCount)
+		thisWeekDeviceCount    *[]DeviceCount          = new([]DeviceCount)
+		thisWeekDOWDeviceCount *[]DeviceDayOfWeekCount = new([]DeviceDayOfWeekCount)
+		url                    *url.URL                = new(url.URL)
+		vd                     *VisitDashboard         = new(VisitDashboard)
+	)
+
+	if err := vrs.uc.Get(ctx, code, url); err != nil {
+		if err := vrs.ur.FindByCode(ctx, code, url); err != nil {
+			return nil, err
+		}
+	}
+
+	if url.UserID == nil {
+		return nil, customerror.NewError(
+			"Forbidden",
+			errors.New("url does not belong to anyone"),
+			customerror.ForbiddenAction,
+		)
+	}
+
+	if *url.UserID != userID {
+		return nil, customerror.NewError(
+			"Forbidden",
+			errors.New("url does not belong to the user"),
+			customerror.ForbiddenAction,
+		)
+	}
+
+	// if exists on cache, return immediately, otherwise fetch from DB and refresh the cache
+	if err := vrs.vc.Get(ctx, code, vd); err == nil {
+		return vd, nil
+	}
+
+	g, newCtx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		return vrs.vrr.GetTodayCount(newCtx, url.ID, todayVisitCount)
+	})
+	g.Go(func() error {
+		return vrs.vrr.GetThisWeekCount(newCtx, url.ID, thisWeekCount)
+	})
+	g.Go(func() error {
+		return vrs.vrr.GetTodayCountGroupByDevice(newCtx, url.ID, todayDeviceCount)
+	})
+	g.Go(func() error {
+		return vrs.vrr.GetThisDayOfWeekCount(newCtx, url.ID, thisDayOfWeekCount)
+	})
+	g.Go(func() error {
+		return vrs.vrr.GetThisWeekCountGroupByDevice(newCtx, url.ID, thisWeekDeviceCount)
+	})
+	g.Go(func() error {
+		return vrs.vrr.GetThisWeekCountGroupByDeviceAndDayOfWeek(newCtx, url.ID, thisWeekDOWDeviceCount)
+	})
+
+	if err := g.Wait(); err != nil {
+		return nil, err
+	}
+
+	*vd = VisitDashboard{
+		TodayVisitCount:        *todayVisitCount,
+		ThisWeekCount:          *thisWeekCount,
+		TodayDeviceCount:       *todayDeviceCount,
+		ThisDayOfWeekCount:     *thisDayOfWeekCount,
+		ThisWeekDeviceCount:    *thisWeekDeviceCount,
+		ThisWeekDOWDeviceCount: *thisWeekDOWDeviceCount,
+	}
+
+	go func(cd string, v VisitDashboard) {
+		ttl := 30 * time.Second
+		conCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		fun := func() error {
+			return vrs.vc.Set(conCtx, cd, ttl, v)
+		}
+
+		services.WithErrorRetry(conCtx, fun, 100*time.Millisecond)
+	}(code, *vd)
+
+	return vd, nil
+}
